665b9bdd1aa8110b465f2448cbb90f72b14dfa58
Shuvo <mek234778@gmail.com>
2025-12-12 00:39:21 +0600

    hashed password

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ const app = express();
 app.use(express.json());
 
 app.post("/auth/sign-up", (req, res) => {
-  console.log()
-  res.send("Sign up");
+  if (req.body.firstName.length<=2)
+  return res.status(400).json({message:"First name must be longer than 2 characters"})
 });
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
