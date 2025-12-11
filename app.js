ffe608b6be5cd4a9a00712e25fd8f6b89e4aaf9c
Shuvo <mek234778@gmail.com>
2025-12-12 00:48:36 +0600

    zod installation

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 import express from "express";
+const { z } = require("zod");
 const app = express();
 
 app.use(express.json());
 
 app.post("/auth/sign-up", (req, res) => {
-  if (req.body.firstName.length<=2)
-  return res.status(400).json({message:"First name must be longer than 2 characters"})
+  if (req.body.firstName.length <= 2)
+    return res
+      .status(400)
+      .json({ message: "First name must be longer than 2 characters" });
 });
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
